3be852bd4e937cd94a19c7183ade8b908e11f5be
eaidig <soydignelson@gmail.com>
2025-11-21 17:08:05 -0300

    refactor: Remove React dependencies and switch to Vanilla JS
    The project has been refactored to run entirely as a single-file Vanilla JavaScript application embedded within `index.html`. This change removes React and ReactDOM from dependencies and effectively disables the React build process.

--- index.tsx
+++ index.tsx
@@ -1,15 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This file is intentionally empty to disable the React build process.
+// The application is now a Single File Vanilla JS app located in index.html.
+console.log("Application running via index.html (Vanilla JS mode)");
